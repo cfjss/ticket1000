77041adec071a7cabe8c8c15fc554ce17aef6c52
am <am@local>
2019-12-17 06:29:47 +0100

    am

--- js/exchange-rates.js
+++ js/exchange-rates.js
@@ -1,13 +1,16 @@
 $.get('https://api.exchangeratesapi.io/latest?symbols=TRY,USD').then(function(data){
 	eurdiff = 0.02;
 	usddiff = 0.03;
+	
 	eurtry = data.rates.TRY;
 	eurusd = data.rates.USD;
-    usdtry = Math.round(eurtry/eurusd * 100)/100;
-	eurBuy  = eurtry - eurdiff;
-	eurSell = eurtry + eurdiff;
-	usdBuy  = usdtry - usddiff;
-	usdSell = usdtry + usddiff;
+    usdtry = eurtry/eurusd;
+	
+	eurBuy  = Math.round((eurtry - eurdiff) * 100) / 100;
+	eurSell = Math.round((eurtry + eurdiff) * 100) / 100;
+	usdBuy  = Math.round((usdtry - usddiff) * 100) / 100;
+	usdSell = Math.round((usdtry + usddiff) * 100) / 100;
+	
     $('#eur-buy').text(eurBuy);
     $('#eur-sell').text(eurSell);
     $('#usd-buy').text(usdBuy);
