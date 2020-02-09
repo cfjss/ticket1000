5d91fe2ca02b4fb8f1b1e9da8b85c9988eea0f6c
am <am@local>
2020-02-09 19:03:59 +0100

    am

---
+++ js/events-popup.js
@@ -0,0 +1,8 @@
+$('.info.alt a[href$="Shopping.aspx?ID=MjM="]').click(function(e){
+  e.preventDefault();
+  $('#modal1').modal();
+})
+$('.info.alt a[href$="Shopping.aspx?ID=MjQ="]').click(function(e){
+  e.preventDefault();
+  $('#modal2').modal();
+});
